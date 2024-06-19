226ff5a27d8671a48e5cd019349d691dd7ac24e9
Belluable <yuojin1004@naver.com>
2024-06-19 16:49:41 +0900

    react components

---
+++ react/rbvite/src/components/Hello.jsx
@@ -0,0 +1,15 @@
+// import PropTypes from 'prop-types';
+
+// Hello.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   age: PropTypes.number,
+// };
+
+export default function Hello(props) {
+  return (
+    <h1 onClick={props.plusCount} style={{ cursor: 'pointer' }}>
+      Hello, {props.name}
+      <small className="font-sm text-red-500">({props.age})</small>!
+    </h1>
+  );
+}
