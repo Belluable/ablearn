54daf562d14c228d01ee7ad0f0f241f782a6d3c3
Belluable <yuojin1004@naver.com>
2024-06-24 17:13:55 +0900

    login, add&edit

--- react/rbvite/src/App.jsx
+++ react/rbvite/src/App.jsx
@@ -3,8 +3,9 @@ import './App.css';
 import Hello from './components/Hello';
 import My from './components/My';
 
+// mock
 const SampleSession = {
-  loginUser: { id: 1, name: 'Hong', age: 26 },
+  // loginUser: { id: 1, name: 'Hong', age: 26 },
   cart: [
     { id: 100, name: '라면', price: 3000 },
     { id: 101, name: '컵라면', price: 2000 },
@@ -15,12 +16,45 @@ const SampleSession = {
 function App() {
   const [session, setSession] = useState(SampleSession);
   const [count, setCount] = useState(0);
-  const plusCount = () => setCount(count + 1);
+  const plusCount = () => setCount((count) => count + 1);
 
   const logout = () => {
     setSession({ ...session, loginUser: null });
   };
 
+  const login = (name) => {
+    const id = 1;
+    const age = 26;
+    const x = {
+      ...session,
+      loginUser: { ...session.loginUser, id, name, age },
+    };
+    setSession(x);
+  };
+
+  const removeItem = (itemId) => {
+    setSession({
+      ...session,
+      cart: [...session.cart.filter((item) => item.id !== itemId)],
+    });
+  };
+
+  const addItem = (name, price) => {
+    const id = Math.max(...session.cart?.map((item) => item.id)) ?? 0;
+    const item = { id: id + 1, name, price };
+    console.log('id:', id);
+    setSession({ ...session, cart: [...session.cart, item] });
+  };
+
+  const saveItem = (id, name, price) => {
+    const editingItem = { id, name, price };
+    setSession({
+      ...session,
+      cart: [
+        ...session.cart.map((item) => (item.id === id ? editingItem : item)),
+      ],
+    });
+  };
   return (
     <>
       <div>
@@ -33,7 +67,14 @@ function App() {
         )}
       </div>
 
-      <My session={session} signOut={logout} />
+      <My
+        session={session}
+        signOut={logout}
+        signIn={login}
+        removeItem={removeItem}
+        addItem={addItem}
+        saveItem={saveItem}
+      />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- react/rbvite/src/components/Login.jsx
+++ react/rbvite/src/components/Login.jsx
@@ -1,73 +1,57 @@
-export default function Login() {
+import { useEffect, useRef } from 'react';
+import Button from './atoms/Button';
+import Input from './atoms/Input';
+
+export default function Login({ singIn }) {
+  const nameRef = useRef();
+  const passwdRef = useRef();
+
+  const login = (evt) => {
+    evt.preventDefault();
+    // evt.stopPropagation();
+    // console.log("nameRef.current.value>>", nameRef.current.value);
+    const name = nameRef.current.value;
+    const passwd = passwdRef.current.value;
+    if (!name || !passwd) {
+      alert('이름과 패스워드를 정확히 입력하세요!');
+      if (!name) nameRef.current.focus();
+      else passwdRef.current.focus();
+      return;
+    }
+
+    singIn(nameRef.current.value);
+  };
+
+  useEffect(() => {
+    if (nameRef.current) nameRef.current.focus();
+  }, [nameRef]);
+
   return (
-    <>
-      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in
-          </h2>
+    <form className="border border-red-300 p-5">
+      <h2 className="text-center text-3xl font-semibold leading-7 text-green-500">
+        Sign In
+      </h2>
+
+      <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="sm:col-span-3">
+          <Input label="Name" ref={nameRef} />
+          {/* <input type="text" ref={nameRef} className="px-2 ring-1 ring-inset" /> */}
         </div>
 
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form class="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                for="email"
-                class="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Name
-              </label>
-              <div class="mt-2">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  required
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                ></input>
-              </div>
-            </div>
-
-            <div>
-              <div class="flex items-center justify-between">
-                <label
-                  for="password"
-                  class="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div class="text-sm">
-                  <a
-                    href="#"
-                    class="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div class="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="current-password"
-                  required
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                ></input>
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
+        <div className="sm:col-span-3">
+          <Input label="Password" type="password" ref={passwdRef} />
         </div>
       </div>
-    </>
+
+      <div className="mt-3 flex justify-center">
+        <Button
+          onClick={login}
+          text="로그인"
+          type="primary"
+          size="lg"
+          className="px-5 py-1"
+        />
+      </div>
+    </form>
   );
 }
